56ae591cf3e6ccdf64702b5fc08a93f09bc92d4b
karimi <skarimi68@gmail.com>
2021-09-29 13:18:54 +0330

    SE-20 prepare icon button for share

--- src/components/icon-button/icon-button.tsx
+++ src/components/icon-button/icon-button.tsx
@@ -2,30 +2,38 @@ import React, { memo } from 'react';
 
 import Link, { LinkProps } from '../link/link';
 
+function IconButtonBase(props: { body?: string; image: string }) {
+  return (
+    <div
+      className={`${'icon-button-container'} ${
+        props.body ? 'icon-button-large' : 'icon-button-small'
+      }`}
+    >
+      <div className={'icon-button-body'}>
+        <div className={'icon-button-icon'}>
+          <img
+            src={props.image}
+            alt={''}
+            width={props.body ? 16 : 24}
+            height={props.body ? 16 : 24}
+          />
+        </div>
+        <p className={'icon-button-text'}>{props.body}</p>
+      </div>
+    </div>
+  );
+}
+
 export default memo(function IconButton(props: {
   body?: string;
   image: string;
-  link: LinkProps;
+  link: LinkProps | false;
 }) {
-  return (
+  return props.link === false ? (
+    <IconButtonBase {...props} />
+  ) : (
     <Link {...props.link}>
-      <div
-        className={`${'icon-button-container'} ${
-          props.body ? 'icon-button-large' : 'icon-button-small'
-        }`}
-      >
-        <div className={'icon-button-body'}>
-          <div className={'icon-button-icon'}>
-            <img
-              src={props.image}
-              alt={''}
-              width={props.body ? 16 : 24}
-              height={props.body ? 16 : 24}
-            />
-          </div>
-          <p className={'icon-button-text'}>{props.body}</p>
-        </div>
-      </div>
+      <IconButtonBase {...props} />
     </Link>
   );
 });
