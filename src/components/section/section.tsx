4cdab109eb43979535f28cc803f6f1eee51b2425
karimi <skarimi68@gmail.com>
2021-09-30 10:47:06 +0330

    SE-28 add section

---
+++ src/components/section/section.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import RightArrow from 'url:../../static/images/shape/right-arrow.svg';
+import Done from 'url:../../static/images/shape/done.svg';
+
+type SectionItem = {
+  text: string;
+  link: string;
+  done: boolean;
+};
+
+export default function Section(props: { items: SectionItem[] }) {
+  return (
+    <div className={'section-container'}>
+      {props.items.map((item: SectionItem, index: number) => {
+        return (
+          <div key={'section-' + index} className={'section'}>
+            <div className={'section-row'}>
+              <a href={item.link}>
+                <div className={'section-link'}>
+                  <p>{item.text}</p>
+                  <img
+                    src={item.done ? Done : RightArrow}
+                    alt={item.done ? 'Done' : '>'}
+                  />
+                </div>
+              </a>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
