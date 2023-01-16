a88be08edb4c28b77e6dbc5d2df6e05b15ccad44
AliReza Seyfpour <a.seyfpour@gmail.com>
2023-01-16 15:46:20 +0000

    SE-121 Make sure earn more is limited to verified
    Improve verified users only popup status
    Restricted earn more save to verified users

--- src/components/verification/verified-users-only.tsx
+++ src/components/verification/verified-users-only.tsx
@@ -1,27 +1,18 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
 import { RouteToPages } from '../../paths';
 import { Button } from '../button/button';
 import { closePopup } from '../popup/popup';
 
-export function VerifiedUsersOnly(): JSX.Element {
+export function VerifiedUsersOnly(props: {
+  header: string;
+  body: JSX.Element;
+}): JSX.Element {
   return (
     <>
-      <div className="small-popup-title title">Get verified to Earn More!</div>
+      <div className="small-popup-title title">{props.header}</div>
       <div className="small-popup-separator" />
-      <div className="small-popup-content">
-        Earn More is only available for verified Swash members.
-        <br />
-        <br />
-        Now live with Swash ads and a fully customisable New Tab Experience, new
-        ways to Earn More will be added over time, including surveys, videos,
-        and technical and commercial product integrations with Swash partners.
-        <br />
-        <br />
-        To use the Earn More feature, your profile has to be verified. Take a
-        few minutes to verify your profile and get ready to enter a new
-        internet.
-      </div>
+      <div className="small-popup-content">{props.body}</div>
       <div className="small-popup-separator" />
       <div className="small-popup-actions">
         <Button
