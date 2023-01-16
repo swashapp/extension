a185f7a41090fd4c0d80a08b8a68b61402d8bb5c
AliReza Seyfpour <a.seyfpour@gmail.com>
2023-01-16 08:33:56 +0000

    SE-121 Minor improvement
    Add new icons
    Fix some mobile layout issue
    Fix code format

--- src/components/new-tab/customisation.tsx
+++ src/components/new-tab/customisation.tsx
@@ -2,6 +2,29 @@ import React, { useCallback, useState } from 'react';
 
 import { helper } from '../../core/webHelper';
 
+const colorList = [
+  'rgb(21, 30, 154)',
+  'rgb(33, 151, 249)',
+  'rgb(31, 195, 220)',
+  'rgb(8, 101, 130)',
+  'rgb(103, 212, 180)',
+  'rgb(7, 125, 90)',
+  'rgb(60, 121, 11)',
+  'rgb(175, 206, 87)',
+  'rgb(240, 203, 68)',
+  'rgb(242, 138, 41)',
+  'rgb(252, 121, 143)',
+  'rgb(193, 34, 110)',
+  'rgb(250, 181, 238)',
+  'rgb(192, 196, 255)',
+  'rgb(150, 119, 238)',
+  'rgb(84, 51, 176)',
+  'rgb(74, 0, 12)',
+  'rgb(91, 92, 99)',
+  'rgb(36, 36, 36)',
+  'rgb(0, 0, 0)',
+];
+
 export function Customisation(props: {
   onBackgroundChange: () => void;
 }): JSX.Element {
@@ -16,150 +39,24 @@ export function Customisation(props: {
       <div className={'bg-options'}>
         <div
           className={'bg-option'}
+          style={{
+            backgroundImage: 'url(/static/images/logos/unsplash.png)',
+            backgroundSize: 'contain',
+          }}
           onClick={() => {
             setBackground('unsplash');
           }}
         ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(21, 30, 154)' }}
-          onClick={() => {
-            setBackground('rgb(21, 30, 154)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(33, 151, 249)' }}
-          onClick={() => {
-            setBackground('rgb(33, 151, 249)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(31, 195, 220)' }}
-          onClick={() => {
-            setBackground('rgb(31, 195, 220)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(8, 101, 130)' }}
-          onClick={() => {
-            setBackground('rgb(8, 101, 130)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(103, 212, 180)' }}
-          onClick={() => {
-            setBackground('rgb(103, 212, 180)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(7, 125, 90)' }}
-          onClick={() => {
-            setBackground('rgb(7, 125, 90)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(60, 121, 11)' }}
-          onClick={() => {
-            setBackground('rgb(60, 121, 11)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(175, 206, 87)' }}
-          onClick={() => {
-            setBackground('rgb(175, 206, 87)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(240, 203, 68)' }}
-          onClick={() => {
-            setBackground('rgb(240, 203, 68)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(242, 138, 41)' }}
-          onClick={() => {
-            setBackground('rgb(242, 138, 41)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(252, 121, 143)' }}
-          onClick={() => {
-            setBackground('rgb(252, 121, 143)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(193, 34, 110)' }}
-          onClick={() => {
-            setBackground('rgb(193, 34, 110)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(250, 181, 238)' }}
-          onClick={() => {
-            setBackground('rgb(250, 181, 238)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(192, 196, 255)' }}
-          onClick={() => {
-            setBackground('rgb(192, 196, 255)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(150, 119, 238)' }}
-          onClick={() => {
-            setBackground('rgb(150, 119, 238)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(84, 51, 176)' }}
-          onClick={() => {
-            setBackground('rgb(84, 51, 176)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(74, 0, 12)' }}
-          onClick={() => {
-            setBackground('rgb(74, 0, 12)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(91, 92, 99)' }}
-          onClick={() => {
-            setBackground('rgb(91, 92, 99)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(36, 36, 36)' }}
-          onClick={() => {
-            setBackground('rgb(36, 36, 36)');
-          }}
-        ></div>
-        <div
-          className={'bg-option'}
-          style={{ background: 'rgb(0, 0, 0)' }}
-          onClick={() => {
-            setBackground('rgb(0, 0, 0)');
-          }}
-        ></div>
+        {colorList.map((color, index) => (
+          <div
+            key={`color-${index}`}
+            className={'bg-option'}
+            style={{ background: color }}
+            onClick={() => {
+              setBackground(color);
+            }}
+          ></div>
+        ))}
       </div>
     );
   }
@@ -168,7 +65,7 @@ export function Customisation(props: {
     <>
       <div className="popup-title title">Customisation</div>
       <div className="popup-separator" />
-      <div className="popup-content-row">
+      <div className="popup-content-sidenav">
         <div className={'customisation-nav'}>
           <div className={'settings-option'}>Background</div>
         </div>
--- src/pages/new-tab.tsx
+++ src/pages/new-tab.tsx
@@ -191,7 +191,7 @@ export default function NewTab(): JSX.Element {
     <>
       <div className={'container'} style={{ ...style }}>
         <Popup />
-        <div className={'item'}>
+        <div className={'row-1'}>
           <div className={'item-actions'}>
             {/*<div>*/}
             {/*  <img src={'/static/images/icons/new-tab/settings.png'} alt={''} />*/}
@@ -242,70 +242,94 @@ export default function NewTab(): JSX.Element {
               <div>Customise</div>
             </div>
           </div>
+          <div className={'item-copyright'}>
+            {bg === 'unsplash' ? (
+              <>
+                Photo by{' '}
+                <a
+                  href={`${credit.userLink}?utm_source=Swash&utm_medium=referral`}
+                  target={'_blank'}
+                  rel={'noreferrer'}
+                >
+                  {credit.userName}
+                </a>{' '}
+                on{' '}
+                <a
+                  href={
+                    'https://unsplash.com/?utm_source=Swash&utm_medium=referral'
+                  }
+                  target={'_blank'}
+                  rel={'noreferrer'}
+                >
+                  Unsplash
+                </a>
+              </>
+            ) : (
+              ''
+            )}
+          </div>
         </div>
-        <div className={'item'}></div>
-        <div className={'item-copyright'}>
-          {bg === 'unsplash' ? `Photo by ${credit.userName} on Unsplash` : ''}
+        <div className={'row-2'}>
+          <div className={'item-search'}>
+            {hide ? (
+              <></>
+            ) : (
+              <>
+                <form
+                  className={'search-form'}
+                  role="search"
+                  action="https://www.google.com/search"
+                >
+                  <input
+                    className={'search-input'}
+                    type="search"
+                    id="query"
+                    name="q"
+                    placeholder="Search..."
+                    aria-label="Search through site content"
+                  />
+                  <button className={'search-button'}>
+                    <svg className={'search-svg'} viewBox="0 0 1024 1024">
+                      <path
+                        className="path1"
+                        d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"
+                      ></path>
+                    </svg>
+                  </button>
+                </form>
+                <div className={'fav-sites'}>{getSites()}</div>
+              </>
+            )}
+          </div>
         </div>
-        <div className={'item-ads'}>
-          <DisplayAds width={300} height={250} />
-        </div>
-        <div className={'item-search'}>
-          {hide ? (
-            <></>
-          ) : (
-            <>
-              <form
-                className={'search-form'}
-                role="search"
-                action="https://www.google.com/search"
-              >
-                <input
-                  className={'search-input'}
-                  type="search"
-                  id="query"
-                  name="q"
-                  placeholder="Search..."
-                  aria-label="Search through site content"
-                />
-                <button className={'search-button'}>
-                  <svg className={'search-svg'} viewBox="0 0 1024 1024">
-                    <path
-                      className="path1"
-                      d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"
-                    ></path>
-                  </svg>
-                </button>
-              </form>
-              <div className={'fav-sites'}>{getSites()}</div>
-            </>
-          )}
-        </div>
-        <div className={'item'}></div>
-        <div className={'item'}></div>
-        <div className={'item-clock'}>
-          {hide ? (
-            <></>
-          ) : (
-            <>
-              <div className={'time'}>
-                {time.toLocaleTimeString([], {
-                  hour: '2-digit',
-                  minute: '2-digit',
-                  second: '2-digit',
-                  hourCycle: 'h24',
-                })}
-              </div>
-              <div className={'date'}>
-                {time.toLocaleDateString([], {
-                  weekday: 'long',
-                  day: '2-digit',
-                  month: 'long',
-                  year: 'numeric',
-                })}
-              </div>
-            </>
-          )}
+        <div className={'row-3'}>
+          <div className={'item-ads'}>
+            <DisplayAds width={300} height={250} />
+          </div>
+          <div className={'item-clock'}>
+            {hide ? (
+              <></>
+            ) : (
+              <>
+                <div className={'time'}>
+                  {time.toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hourCycle: 'h24',
+                  })}
+                </div>
+                <div className={'date'}>
+                  {time.toLocaleDateString([], {
+                    weekday: 'long',
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric',
+                  })}
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
