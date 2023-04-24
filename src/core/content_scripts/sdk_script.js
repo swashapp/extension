366e8c813e2421eac67c7bc3d1420b1ba4195a29
AliReza Seyfpour <a.seyfpour@gmail.com>
2023-04-24 13:46:51 +0330

    Added second solution for message passing

--- src/core/content_scripts/sdk_script.js
+++ src/core/content_scripts/sdk_script.js
@@ -9,11 +9,21 @@ var sdkScript = (function () {
     profile = messages;
     embed(codeToInject);
 
-    document.addEventListener('getUserInfo', function () {
+    // Solution #1
+    // document.addEventListener('getUserInfo', function () {
+    //   console.log('received request');
+    //   document.dispatchEvent(
+    //     new CustomEvent('getUserInfoResponse', { detail: profile }),
+    //   );
+    // console.log('sent response');
+    // });
+    // Solution #2
+    window.addEventListener('message', (event) => {
+      if (event.data !== 'getUserInfo') return;
       console.log('received request');
-      document.dispatchEvent(
-        new CustomEvent('getUserInfoResponse', { detail: profile }),
-      );
+      console.log(event.data);
+      event.source.postMessage(profile, event.origin);
+      console.log('sent response');
     });
   }
 
@@ -24,27 +34,39 @@ var sdkScript = (function () {
   function codeToInject() {
     window.swashSdk = {
       getUserInfo: async () => {
+        // Solution #1
         // return new Promise((resolve) => {
         // document.addEventListener('getUserInfoResponse', function (e) {
         //   console.log('received response');
         //   console.log(e.detail);
         // });
         // document.dispatchEvent(new CustomEvent('getUserInfo'));
+        // console.log('sent request');
         // });
-
-        return {
-          user_id: '1',
-          gender: 'Male',
-          birth: '1988',
-          age: '32-30',
-          income: '50-75K',
-          country: 'Netherlands',
-          city: 'Smilde',
-          marital: 'Married',
-          household: '3',
-          employment: 'Full-time',
-          industry: 'Finance and Economic',
-        };
+        // Solution #2
+        return new Promise((resolve) => {
+          window.addEventListener('message', function (e) {
+            if (e.data === 'getUserInfo') return;
+            console.log('received response');
+            console.log(e);
+            resolve(e.data);
+          });
+          window.postMessage('getUserInfo', '*');
+          console.log('sent request');
+        });
+        // return {
+        //   user_id: '1',
+        //   gender: 'Male',
+        //   birth: '1988',
+        //   age: '32-30',
+        //   income: '50-75K',
+        //   country: 'Netherlands',
+        //   city: 'Smilde',
+        //   marital: 'Married',
+        //   household: '3',
+        //   employment: 'Full-time',
+        //   industry: 'Finance and Economic',
+        // };
       },
     };
   }
@@ -65,7 +87,7 @@ if (typeof window.swashSdk === 'undefined') {
   window.swashSdk = {
     obj: 'sdk',
     func: 'getUserProfile',
-    params: [window.location.href],
+    params: [],
   };
 
   browser.runtime
