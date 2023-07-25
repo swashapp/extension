29e0605b403719841678a49962f865b0291a0fd9
sajjad <skarimi68@gmail.com>
2023-07-25 13:52:37 +0200

    fix: try to fix sdk injection

--- src/core/content_scripts/sdk_script.js
+++ src/core/content_scripts/sdk_script.js
@@ -1,6 +1,6 @@
 var sdkScript = (function () {
   function init() {
-    embed(codeToInject);
+    embed('core/content_scripts/sdk_code_script.js');
     addMessageHandler();
   }
 
@@ -47,47 +47,11 @@ var sdkScript = (function () {
     );
   }
 
-  function codeToInject() {
-    const callFunction = async function (data) {
-      return new Promise((resolve, reject) => {
-        const listener = function (event) {
-          if (event.data.id !== `${data.id}Resp`) return;
-
-          window.removeEventListener('message', listener);
-          if (event.data.response?.error) reject(event.data.response.error);
-          else resolve(event.data.response);
-        };
-
-        window.addEventListener('message', listener);
-        window.postMessage(data, '*');
-      });
-    };
-
-    window.swashSdk = {
-      getStatus: async () => {
-        return callFunction({ id: 'getStatus' });
-      },
-      getUserInfo: async () => {
-        return callFunction({ id: 'getUserInfo' });
-      },
-      getSurveyUrl: async (provider) => {
-        return callFunction({ id: 'getSurveyUrl', params: [provider] });
-      },
-      getSurveyHistory: async (params) => {
-        return callFunction({ id: 'getSurveyHistory', params: [params] });
-      },
-      openPopupPage: async () => {
-        return callFunction({ id: 'openPopupPage' });
-      },
-      openProfilePage: async () => {
-        return callFunction({ id: 'openProfilePage' });
-      },
-    };
-  }
-
-  function embed(fn) {
+  function embed(sdk_code_script) {
+    const url = browser.runtime.getURL(sdk_code_script);
     const script = document.createElement('script');
-    script.text = `(${fn.toString()})();`;
+    script.setAttribute('type', 'text/javascript');
+    script.setAttribute('src', url);
     document.documentElement.appendChild(script);
   }
 
