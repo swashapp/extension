de402ea3a64f5d040b35c2a3bce0f7863b8ee1a9
AliReza Seyfpour <a.seyfpour@gmail.com>
2025-02-26 10:47:31 +0330

    fix: fix an issue in text masking

--- src/core/data/initial-configuration.ts
+++ src/core/data/initial-configuration.ts
@@ -38,7 +38,7 @@ import { FacebookModule } from "./modules/social/facebook.module";
 import { TwitterModule } from "./modules/social/twitter.module";
 
 export const InitialConfiguration: ConfigurationStorage = {
-  version: "4",
+  version: "5",
 
   apis: {
     ads: AdsApi,
